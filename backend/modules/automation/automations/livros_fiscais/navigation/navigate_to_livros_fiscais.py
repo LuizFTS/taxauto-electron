1ad8ecb442a87be1b42c42b833784d3a8b63c732
Luiz Filipe <luiz-@outlook.de>
2026-03-09 18:16:52 -0300

    feat: enhance ERP session management and navigation
    Implements functionality to programmatically focus and maximize the ERP window, improving the reliability of UI automation. Additionally, specific keyboard and text input commands are added to navigate directly to the 'Livros Fiscais' screen within the ERP application.

--- backend/modules/automation/automations/livros_fiscais/navigation/navigate_to_livros_fiscais.py
+++ backend/modules/automation/automations/livros_fiscais/navigation/navigate_to_livros_fiscais.py
@@ -1,10 +1,15 @@
+import nazm as a
+
 class NavigateToLivrosFiscais:
 
-    def execute(self, session):
+    def execute(self):
         print("[NAVIGATION] Navigating to Livros Fiscais screen")
         # open ERP
         # login
         # navigate menu
         # open livros fiscais screen
+        a.press('f9')
+        a.type('OBFI0200', 0.001)
+        a.press('enter')
 
-        pass
+        
