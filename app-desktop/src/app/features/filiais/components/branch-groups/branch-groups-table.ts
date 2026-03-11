6d70ada61b0dfa2e371382aabfb3e037008de7c4
Luiz Filipe <luiz-@outlook.de>
2026-03-11 19:13:20 -0300

    feat: add scaffold for BranchGroupsTable component
    This commit introduces the foundational files for a new Angular component, `BranchGroupsTable`. It includes the component's TypeScript definition, a basic HTML template with a placeholder paragraph, and an empty SCSS file for styling. This setup provides the minimal structure for developing features related to branch group management.

---
+++ app-desktop/src/app/features/filiais/components/branch-groups/branch-groups-table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branch-groups-table',
+  imports: [],
+  templateUrl: './branch-groups-table.html',
+  styleUrl: './branch-groups-table.scss',
+})
+export class BranchGroupsTable {}
