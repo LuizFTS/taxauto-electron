3dc030025ed2fdeaf7b227c6276db6f3e3f70433
Luiz Filipe <luiz-@outlook.de>
2026-03-21 09:49:51 -0300

    feat: add generic branch selection modal component
    Implemented a new Angular component `SelectBranchesModal` to allow users to select multiple branches based on predefined groups, company filters, and search queries. This modal is reusable and provides functionalities like selecting all, clearing selection, and displaying branches grouped by company or individual selection.

---
+++ app-desktop/src/app/shared/components/select-branches-modal/select-branches-modal.ts
@@ -0,0 +1,222 @@
+import { Component, computed, inject, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  ModalService,
+  BranchService,
+  type GrupoEmpresas,
+  type Filial,
+  CompanyService,
+  type Empresa,
+} from '../../../core';
+import { BehaviorSubject, combineLatest, map, Observable, of, switchMap } from 'rxjs';
+import { CompanyMapper } from '../../../shared/mappers/company.mapper';
+import { BranchGroupService } from '../../../core/services/api/data/branch-group.service';
+import { GroupBranchMapper } from '../../../shared/mappers/group-branch.mapper';
+import { Select } from '../../../shared';
+
+@Component({
+  selector: 'app-select-branches-modal',
+  imports: [CommonModule, FormsModule, Select],
+  templateUrl: './select-branches-modal.html',
+  styleUrl: './select-branches-modal.scss',
+})
+export class SelectBranchesModal implements OnInit {
+  @Input() title = 'Seleção de filiais';
+  @Input() subtitle = 'Selecione os grupos ou filiais individuais para execução';
+  @Input() onConfirm?: (branches: string[]) => void;
+  @Input() filiais?: string[];
+
+  tiposLivro$!: Observable<{ value: number | null; label: string }[]>;
+
+  private modalService = inject(ModalService);
+  private branchService = inject(BranchService);
+  private companyService = inject(CompanyService);
+  private groupService = inject(BranchGroupService);
+
+  private companyFilterSubject = new BehaviorSubject<number | null>(null);
+  private filiaisSubject = new BehaviorSubject<Filial[]>([]);
+  private empresasSubject = new BehaviorSubject<Empresa[]>([]);
+  private readonly filiaisFiltered = computed(() => {
+    const list = this.filiaisSubject.value;
+    const q = this.searchSubject.value;
+    const selectedId = this.companyFilterSubject.value;
+    return list
+      .filter((f) => {
+        const branchNumber = parseInt(f.numero, 10);
+        const numberMatch = !isNaN(branchNumber) && branchNumber.toString().includes(q);
+        const ufMatch = f.uf.toLowerCase().includes(q.toLowerCase());
+
+        return numberMatch || ufMatch;
+      })
+      .filter((f) => {
+        if (selectedId === null || selectedId === undefined) {
+          return true;
+        }
+        return parseInt(f.empresaId) === selectedId;
+      });
+  });
+
+  branches$!: Observable<Filial[]>;
+  empresas$!: Observable<Empresa[]>;
+  groups: GrupoEmpresas[] = [];
+
+  selectedBranches: Set<string> = new Set<string>();
+  selectedCompany = '';
+  selectedGroup: string | null = null;
+
+  private searchSubject = new BehaviorSubject<string>('');
+  searchQuery = '';
+
+  onSearchChange(valor: string) {
+    this.searchSubject.next(valor);
+  }
+
+  ngOnInit() {
+    this.companyService
+      .getAll()
+      .pipe(
+        switchMap((companies) =>
+          this.branchService.getAll().pipe(map((branches) => ({ companies, branches }))),
+        ),
+      )
+      .subscribe(({ companies, branches }) => {
+        // Alimentamos os Subjects com os dados iniciais do Mapper
+        this.filiaisSubject.next(CompanyMapper.toFilialList(branches, companies));
+        this.empresasSubject.next(CompanyMapper.toEmpresaList(companies, branches));
+      });
+    this.branches$ = combineLatest([
+      this.filiaisSubject,
+      this.searchSubject,
+      this.companyFilterSubject,
+    ]).pipe(
+      map(([list, q, selectedId]) => {
+        return list
+          .filter((f) => {
+            const branchNumber = parseInt(f.numero, 10);
+            const numberMatch = !isNaN(branchNumber) && branchNumber.toString().includes(q);
+            const ufMatch = f.uf.toLowerCase().includes(q.toLowerCase());
+            return numberMatch || ufMatch;
+          })
+          .filter((f) => {
+            if (selectedId === null || selectedId === undefined) {
+              return true;
+            }
+            return parseInt(f.empresaId) === selectedId;
+          });
+      }),
+    );
+
+    this.empresas$ = this.empresasSubject.asObservable();
+
+    this.tiposLivro$ = this.empresas$.pipe(
+      map((empresas) => {
+        const listaMapeada = empresas.map((empresas) => ({
+          value: parseInt(empresas.id),
+          label: empresas.nome,
+        }));
+
+        return [{ value: null, label: 'Todas as empresas' }, ...listaMapeada];
+      }),
+    );
+
+    this.groupService.getAll().subscribe((data) => {
+      this.groups = GroupBranchMapper.toGroupBranchList(data);
+    });
+
+    if (this.filiais) {
+      this.selectedBranches = new Set<string>(this.filiais);
+    }
+  }
+
+  toggleBranch(branchId: string) {
+    if (this.selectedBranches.has(branchId)) {
+      this.selectedBranches.delete(branchId);
+      // Unselect group if a branch is unselected
+      if (this.selectedGroup) {
+        const group = this.groups.find((g) => g.id === this.selectedGroup);
+
+        if (group && group.branches.some((filial) => filial.id === branchId)) {
+          this.selectedGroup = null;
+        }
+      }
+    } else {
+      this.selectedBranches.add(branchId);
+    }
+  }
+
+  toggleGroup(groupId: string) {
+    if (this.selectedGroup === groupId) {
+      this.selectedGroup = null;
+      this.clearSelection();
+    } else {
+      this.selectedGroup = groupId;
+      const group = this.groups.find((g) => g.id === groupId);
+      if (group) {
+        this.selectedBranches.clear();
+        group.branches.forEach((bId) => this.selectedBranches.add(bId.codigo.padStart(3, '0')));
+      }
+    }
+  }
+
+  selectAll() {
+    this.selectedGroup = null;
+
+    const visibleBranches = this.filiaisFiltered();
+
+    if (visibleBranches) {
+      visibleBranches.forEach((b) => {
+        if (b.numero) {
+          this.selectedBranches.add(b.numero);
+        }
+      });
+    }
+  }
+
+  clearSelection() {
+    this.selectedGroup = null;
+    this.selectedBranches.clear();
+  }
+
+  confirm() {
+    if (this.onConfirm) {
+      this.onConfirm(Array.from(this.selectedBranches));
+    }
+    this.modalService.close();
+  }
+
+  cancel() {
+    this.modalService.close();
+  }
+
+  onCompanySelect(event: { value: number | null; label: string }) {
+    this.selectedCompany = event.value?.toString() || '';
+    this.selectedBranches.clear();
+    this.selectedGroup = null;
+    this.companyFilterSubject.next(event.value);
+  }
+
+  getNomeEmpresaSelecionada(): Observable<string> {
+    if (this.selectedCompany === '') {
+      return of('Todas as empresas');
+    }
+    return this.empresasSubject.asObservable().pipe(
+      map((empresas) => {
+        const empresa = empresas.find((e) => e.id === this.selectedCompany);
+        return empresa ? empresa.nome : '';
+      }),
+    );
+  }
+
+  formatGroupName(value: string) {
+    const firstWord = value.split(' ')[0];
+    const remainingWords = value.split(' ').slice(1).join(' ');
+
+    return (
+      firstWord.charAt(0).toUpperCase() +
+      firstWord.slice(1).toLowerCase() +
+      ' ' +
+      remainingWords.toUpperCase()
+    );
+  }
+}
